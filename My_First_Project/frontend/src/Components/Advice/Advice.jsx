d4ceed68977281e3a0ef26a4db7015213e8346ae
Pwnisht <118707759+pwnisht-7@users.noreply.github.com>
2023-11-29 11:21:27 +0545

    complete

--- My_First_Project/frontend/src/Components/Advice/Advice.jsx
+++ My_First_Project/frontend/src/Components/Advice/Advice.jsx
@@ -1,10 +1,303 @@
-
+import Hero from "./Group 8.png";
+import V1 from "./Vector(3).png";
+import V2 from "./Vector(4).png";
+import scan from './Scan.png'
+import B1 from "./BRUSHES.png";
+import { FaMobileAlt } from "react-icons/fa";
+import Mid from "./vecteezy_spices-and-herbs-on-a-transparent-background_13526617_618 1(1).png";
+import B2 from "./BRUSHES(1).png";
 const Advice = () => {
   return (
-    <div>
-      This Is Allergy Advice
-    </div>
-  )
-}
+    <>
+      <section className="w-[1440px] h-[611px] flex items-center justify-center gap-20 py-14 px-36">
+        <div className="flex flex-col gap-10 w-[576px] pb-16">
+          <h1 className="flex font-[allura] text-[#0c6969] text-6xl">
+            Allergy Advices
+          </h1>
+          <div className="flex flex-col gap-6">
+            <h4 className="flex text-2xl tracking-widest text-slate-500">
+              AT OUR RESTAURNT
+            </h4>
+            <h3 className="flex flex-col text-[30px] font-bold">
+              We Use Only The Freshest and highest quality ingredients in our
+              dishes,
+              <span className="text-orange-600">
+                and offer transparency in our ingredient labeling
+              </span>
+            </h3>
+          </div>
+        </div>
+        <div className="w-[498px] h-[499px] relative">
+          <img src={Hero} alt="HeroImg" />
+          <div className="flex flex-wrap absolute gap-4 h-[192px] -left-10 top-24 w-[166px] -z-10">
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+          </div>
+          <div className="flex flex-wrap absolute gap-4 h-[192px] w-[166px] bottom-0 right-10 -z-10">
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+            <div className="h-[10px] w-[10px] bg-slate-200 rounded-full"></div>
+          </div>
+        </div>
+      </section>
+      <section className="w-[1440px] h-[2600px] flex flex-col gap-14 py-16 px-36 relative">
+        <div className="flex items-center justify-center gap-10 p-10 w-full ">
+          <img src={B1} alt="Brushes" />
+          <h1 className="text-4xl font-bold font-[chela-one]">
+            <span className="text-orange-600">INGREDIENT'S</span> USED
+          </h1>
+          <img src={B2} alt="Brushes 2" />
+        </div>
+        <img src={V1} alt="" className="absolute top-56 left-48" />
+        <img
+          src={Mid}
+          alt=""
+          className="absolute top-[54%] left-0 -translate-y-[50%] h-[1030px]"
+        />
+        <img src={V2} alt="" className="absolute bottom-16 right-28" />
+        <div className="flex gap-8 p-14 rounded-2xl border-4  absolute bg-white border-[#0C6967] left-[50%] w-[744px] -translate-x-[50%] top-56 z-10 flex-col">
+          <h1 className="text-3xl font-bold">For The Dough</h1>
+          <ul className="w-full marker:text-orange-600 font-medium text-2xl list-outside list-disc ml-6">
+            <li className="w-full h-11 pl-2">120 gms Refined Flavour</li>
+            <li className="w-full h-11 pl-2">1/4 Tsp Baking Powder</li>
+            <li className="w-full h-11 pl-2">1/2 Tsp Salt Water</li>
+          </ul>
+        </div>
+        <div className="flex gap-8 p-14 rounded-2xl border-4  absolute bg-white border-[#0C6967] left-[50%] w-[744px] h-[528px] top-[600px] -translate-x-[50%] z-10 flex-col">
+          <h1 className="text-3xl font-bold">For The Chicken Filling</h1>
+          <ul className="w-full marker:text-orange-600 font-medium text-2xl list-outside list-disc ml-6">
+            <li className="w-full h-11 border-b-2">1 Cup Chicken (Minced)</li>
+            <li className="w-full h-11 border-b-2">
+              1/2 Cup Onions Finely Chopped
+            </li>
+            <li className="w-full h-11 border-b-2">1 TBSP Oil</li>
+            <li className="w-full h-11 border-b-2">1/2 Tsp Garlic Paste</li>
+            <li className="w-full h-11 border-b-2">1/2 Tsp Soya Sauce</li>
+            <li className="w-full h-11 border-b-2">
+              1/4 Tsp Black Paper Powder
+            </li>
+            <li className="w-full h-11 border-b-2">Salt</li>
+            <li className="w-full h-11 border-b-2">1/4 Tsp vinegar</li>
+          </ul>
+        </div>
+        <div className="flex gap-8 p-14 rounded-2xl border-4  absolute bg-white border-[#0C6967] left-[50%] bottom-[810px] w-[744px] -translate-x-[50%] z-10 flex-col">
+          <h1 className="text-3xl font-bold">For The Vegetarian Filling</h1>
+          <ul className="w-full marker:text-orange-600 font-medium text-2xl list-outside list-disc ml-6">
+            <li className="w-full h-11 border-b-2">
+              1 Cup Cabbage And Carrots , Grated
+            </li>
+            <li className="w-full h-11 border-b-2">
+              2 Tbsp Onion Finally Chopped
+            </li>
+            <li className="w-full h-11 border-b-2">
+              1/2 Tbsp Garlic Finally Chopped
+            </li>
+            <li className="w-full h-11 border-b-2">1/4 Tbsp Vinegar</li>
+            <li className="w-full h-11 border-b-2">1/2 Soya Sauce</li>
+            <li className="w-full h-11 border-b-2">To Taste Salt</li>
+            <li className="w-full h-11 border-b-2">To Taste Peeper</li>
+            <li className="w-full h-11 border-b-2">To Taste Sugar</li>
+            <li className="w-full h-11 border-b-2">1 Tbsp Corn Flavour</li>
+          </ul>
+        </div>
+        <div className="flex gap-8 p-14 rounded-2xl border-4  absolute bg-white border-[#0C6967] left-[50%] h-[528px] bottom-44 w-[744px] -translate-x-[50%] z-10 flex-col">
+          <h1 className="text-3xl font-bold">For The Chicken Filling</h1>
+          <ul className="w-full marker:text-orange-600 font-medium text-2xl list-outside list-disc ml-6">
+            <li className="w-full h-11 border-b-2">25 Gmz Garlic Pleeded</li>
+            <li className="w-full h-11 border-b-2">6 Gms Whole Red Chillies</li>
+            <li className="w-full h-11 border-b-2">3 Tbsp Vinegar</li>
+            <li className="w-full h-11 border-b-2">3 Tbsp Vinegar</li>
+            <li className="w-full h-11 border-b-2">1 Tbsp Oil</li>
+            <li className="w-full h-11 border-b-2">To Taste Salt</li>
+            <li className="w-full h-11 border-b-2">To Taste Salt</li>
+            <li className="w-full h-11 border-b-2">To Taste Sugar</li>
+          </ul>
+        </div>
+      </section>
+      <section className="w-[1440px] h-[1506px] py-16 px-36 flex flex-col gap-14">
+        <div className="flex items-center justify-center gap-10 p-10 w-full ">
+          <img src={B1} alt="Brushes" />
+          <h1 className="text-4xl font-bold font-[chela-one]">
+            <span className="text-orange-600">Allergy</span> Advice
+          </h1>
+          <img src={B2} alt="Brushes 2" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="text-xl text-slate-700 leading-7 font-normal">
+            Lorem ipsum dolor sit amet consectetur. Feugiat lectus sit est
+            blandit in gravida. Nisl facilisis luctus sit porttitor placerat
+            purus tincidunt. Cursus nascetur a mauris odio quis sociis pretium
+            nisl. In viverra diam dis montes orci congue vulputate magna
+            ullamcorper. Semper tellus ipsum felis maecenas ultrices turpis amet
+            euismod malesuada. Amet sed neque vitae malesuada quis sed urna.
+          </p>
+          <p className="text-xl text-slate-700 leading-7 font-normal">
+            Feugiat orci feugiat cursus risus tincidunt. Aliquet nam iaculis
+            bibendum tortor varius sit. Volutpat nunc nisl quisque elit. Ac est
+            ultricies risus et sed. Donec auctor tristique quam morbi
+            pellentesque et. Scelerisque dui id arcu laoreet iaculis nunc et
+            nulla sed. Aliquet nullam vulputate quis ut mi placerat auctor.
+            Ipsum massa suspendisse netus mollis interdum.
+          </p>
+          <p className="text-xl text-slate-700 leading-7 font-normal">
+            Augue leo eget ut vitae maecenas ac. Metus massa nunc tristique
+            donec dignissim aenean congue justo et. Parturient elit adipiscing
+            non vitae mattis. Congue senectus urna cursus nisi nisl nisl
+            tincidunt rhoncus. Congue sit etiam vestibulum elit aenean leo enim
+            volutpat. Aliquet vitae condimentum bibendum ullamcorper vel cursus
+            libero netus bibendum. Lorem id ut proin tincidunt. Nibh nunc amet
+            odio est et. Sem dui nunc morbi leo quisque viverra ultrices. Mattis
+            at tempus ultricies ut feugiat nunc dignissim. Urna turpis purus
+            turpis elit risus.
+          </p>
+          <p className="text-xl text-slate-700 leading-7 font-normal">
+            Sed elit ornare senectus dignissim diam dolor sed dictum. Faucibus
+            mauris senectus odio blandit diam nunc. Urna volutpat ut potenti ut
+            mus orci neque sem. Mattis amet at amet faucibus nisl morbi aenean
+            ac. Justo turpis bibendum donec lacinia. Enim integer ornare nibh
+            urna enim tortor arcu sit lacinia. Dapibus praesent vitae dolor
+            cursus fringilla hac facilisis pellentesque. Elementum lacus varius
+            et neque amet aliquam donec sed. Ultrices euismod commodo id
+            vehicula commodo. Amet lorem eros nulla mauris. Sit arcu enim sit
+            nunc mi sed aenean. Ornare enim vitae volutpat est dui tortor.
+            Viverra habitant et consequat vitae elit aliquet iaculis in
+            tristique.
+          </p>
+          <p className="text-xl text-slate-700 leading-7 font-normal">
+            Quam ipsum viverra ac laoreet platea in praesent. Mattis molestie
+            arcu nulla convallis interdum proin dui ut porta. Amet potenti
+            praesent pulvinar scelerisque vitae habitant eget. Senectus hac id
+            aliquam ipsum urna. Est ut amet nec sollicitudin nunc viverra nec
+            ipsum suspendisse. Etiam risus ultricies dictum nisl. Bibendum
+            ultrices elementum feugiat erat suspendisse mi fermentum magna
+            suspendisse. Nisi mattis consequat nec donec eu amet nulla a. At
+            blandit vulputate morbi tortor amet. Hac nec gravida tempor ac.
+            Senectus nunc libero scelerisque quisque. Risus condimentum mattis
+            massa integer.
+          </p>
+        </div>
+        <div>
+        <div className="bg-gray-50 p-10 w-full flex flex-col gap-10">
+          <div className="flex justify-center text-center flex-col">
+            <h1 className="text-3xl font-bold text-[#0C6967]">
+              Scan the QR code
+            </h1>
+            <p className="text-slate-600 text-xl">
+              You can also check the allergy advices using your phone as well
+            </p>
+          </div>
+          <div className="flex items-center justify-center flex-col gap-4">
+            <div className="flex items-center justify-center">
+              <img src={scan} alt="Qr Code" />
+            </div>
+            <div className="flex items-center justify-center">
+              <h1 className="text-3xl flex items-end justify-items-end gap-1 font-bold">
+                <span className="text-orange-600">SCAN</span>
+                <span>ME!</span>
+                <span>
+                  <FaMobileAlt />
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Advice
+export default Advice;
